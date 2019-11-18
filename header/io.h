8fd9f3034efc85ffaa263a99700616ebe7acd007
hhuyn019 <38028924+hhuyn019@users.noreply.github.com>
2019-11-17 16:42:15 -0800

    Update io.h

--- header/io.h
+++ header/io.h
@@ -6,5 +6,5 @@ void LCD_ClearScreen(void);
 void LCD_WriteCommand (unsigned char Command);
 void LCD_Cursor (unsigned char column);
 void LCD_DisplayString(unsigned char column ,const unsigned char *string);
-void delay_ms(int ms);
+void delay_ms(int miliSec);
 #endif
